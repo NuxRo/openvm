20ac8ecd2243964949d3d15916aac8d74e7dc6c3
NuxRo <nux@li.nux.ro>
2014-11-25 18:06:00 +0000

    Update centos-update-etc-issue.sh

--- scripts/centos-update-etc-issue.sh
+++ scripts/centos-update-etc-issue.sh
@@ -5,7 +5,7 @@
 
 echo Kernel and hostname: > /etc/issue
 uname -r >> /etc/issue
-hostname -f >> /etc/issue
+hostname >> /etc/issue
 echo >> /etc/issue
 echo IPs, routes, nameservers: >> /etc/issue
 ip a| grep inet >> /etc/issue

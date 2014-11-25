c246c181cc9bd88c511d8d166058e5918e594067
NuxRo <nux@li.nux.ro>
2014-11-25 16:29:17 +0000

    added scripts dir

---
+++ scripts/sysprep.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# script "syspreps" the current install
+
+# remove bash history
+unset HISTFILE
+rm -fv /root/.bash_history
+
+# remove package history and leftovers
+yum clean all
+
+# remove current logs
+find /var/log -type f -delete
+
+# remove udev netweork associations (as macs will change)
+rm -fv /etc/udev/rules.d/*-net.rules /etc/udev/rules.d/*persistent*
+
+# remove ssh host keys, they need to be unique and will be regenerated
+rm -fv /etc/ssh/*key*
+
+# remove random seed, needs to be unique and will be regenerated
+rm -fv /var/lib/random-seed
+

f66a9feda517cc674e860b9bf6c39fb8586e29e9
NuxRo <nux@li.nux.ro>
2014-11-25 16:59:58 +0000

    add PERSISTENT_DHCLIENT=1

--- scripts/centos-sysprep.sh
+++ scripts/centos-sysprep.sh
@@ -15,6 +15,13 @@ find /var/log -type f -delete
 # remove udev netweork associations (as MACs will change)
 rm -fv /etc/udev/rules.d/*-net.rules /etc/udev/rules.d/*persistent*
 
+# remove MACs from ifcfg files
+sed -i '/HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-*
+sed -i '/UUID/d' /etc/sysconfig/network-scripts/ifcfg-*
+
+# make sure PERSISTENT_DHCP is on, we don't want to lose IPs if DHCP server goes AWOL
+for ifcfg in /etc/sysconfig/network-scripts/ifcfg-eth? ; do grep -q PERSISTENT_DHCLIENT=1 $ifcfg || echo PERSISTENT_DHCLIENT=1 >> $ifcfg ; done
+
 # remove ssh host keys, they need to be unique and will be regenerated
 rm -fv /etc/ssh/*key*
 
